cbc92b20f45fa9f5b2680489c4b96a5a4e3d97e4
ShashankkK3 <shashank1208singh@gmail.com>
2025-05-23 04:50:06 +0530

    Time: 17 ms (25.92%), Space: 45.9 MB (20.61%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,30 +1,35 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        final Map<String, Integer> counts = new HashMap<>();
-        for (final String word : words) {
-            counts.put(word, counts.getOrDefault(word, 0) + 1);
-        }
-        final List<Integer> indexes = new ArrayList<>();
-        final int n = s.length(), num = words.length, len = words[0].length();
-        for (int i = 0; i < n - num * len + 1; i++) {
-            final Map<String, Integer> seen = new HashMap<>();
-            int j = 0;
-            while (j < num) {
-                final String word = s.substring(i + j * len, i + (j + 1) * len);
-                if (counts.containsKey(word)) {
-                    seen.put(word, seen.getOrDefault(word, 0) + 1);
-                    if (seen.get(word) > counts.getOrDefault(word, 0)) {
-                        break;
+        List<Integer> ans = new ArrayList<>();
+        int n = s.length();
+        int m = words.length;
+        int w = words[0].length();
+
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String x : words)
+        map.put(x, map.getOrDefault(x,0)+1);
+
+        for(int i=0; i<w; i++){
+            HashMap<String,Integer> temp = new HashMap<>();
+            int count = 0;
+            for(int j=i,k=i; j+w <= n; j=j+w){
+                String word = s.substring(j,j+w);
+                temp.put(word,temp.getOrDefault(word,0)+1);
+                count++;
+                
+                if(count==m){
+                    if(map.equals(temp)){
+                        ans.add(k);
                     }
-                } else {
-                    break;
+                    String remove = s.substring(k,k+w);
+                    temp.computeIfPresent(remove, (a, b) -> (b > 1) ? b - 1 : null);
+                    count--;
+                    k=k+w;
                 }
-                j++;
-            }
-            if (j == num) {
-                indexes.add(i);
-            }
-        }
-        return indexes;
-    }
-}
+            }//inner for loop
+        }//outer for loop
+        return ans;
+    }//method
+}//class
+
+//................................................................
