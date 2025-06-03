6ce51ddbe563695ad016c0576e78e3ae4519e3ee
ShashankkK3 <shashank1208singh@gmail.com>
2025-06-03 06:12:20 +0530

    Time: 1 ms (100%), Space: 57 MB (62.69%) - LeetHub

--- 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.java
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.java
@@ -1,32 +1,32 @@
 class Solution {
-    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 0; i < initialBoxes.length; i++) {
-            if (status[initialBoxes[i]] == 1)
-                dq.addFirst(initialBoxes[i]);
-            else
-                dq.addLast(initialBoxes[i]);
+    public int maxCandies(int[] status,int[] candies,int[][] keys,int[][] containedBoxes,int[] initialBoxes) {
+
+        int count=0; // Total candies collected
+        boolean[] vis=new boolean[status.length]; // Track visited boxes
+        for(int v:initialBoxes){
+            dfs(v,status,keys,containedBoxes,vis);
         }
 
-        int sum = 0;
-        while (!dq.isEmpty()) {
-            int currBox = dq.removeFirst();
-            if (status[currBox] == 0)
-                break;
-
-            sum += candies[currBox];
-
-            for (int key : keys[currBox])
-                status[key] = 1;
-
-            for (int box : containedBoxes[currBox]) {
-                if (status[box] == 1)
-                    dq.addFirst(box);
-                else
-                    dq.addLast(box);
+        for(int i=0;i<candies.length;i++){
+            if(vis[i]&&status[i]==1){
+                count+=candies[i];
             }
         }
+        return count;
+    }
 
-        return sum;
+    public void dfs(int v,int[] status,int[][] keys,int[][] containedBoxes,boolean[] vis){ 
+
+        vis[v]=true; // Mark the current box as visited
+        for(int vKey:keys[v]){
+            if(vKey==v) continue; // Skip self-key
+            status[vKey]=1; // Unlock the box
+        }
+
+        for(int vContained:containedBoxes[v]){
+            if(!vis[vContained]){
+                dfs(vContained,status,keys,containedBoxes,vis);
+            }
+        }
     }
 }
